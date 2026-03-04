f2dfbfa6f993d9a58d1f30154b80a99ee857b7d2
Geethika185 <2400030322@kluniversity.in>
2026-03-04 16:26:25 +0530

    Added Skill-1 inside FSAD-SKILL folder

---
+++ Skill-1/main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+        System.out.println("Feature branch added");
+        System.out.println("Bug fixed in this version");
+
+    }
+}
